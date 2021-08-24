014f5cb4c0e6b5c572bc369124e68886b00e849f
jirolk <sandrocastillo@unc.edu.py>
2021-08-24 11:54:06 -0400

    Primer Scripts

--- controlAccesos.py
+++ controlAccesos.py
@@ -1,34 +1,59 @@
-#! python3
-# import os
-# from time import sleep
-import xlrd
-#carga del documento
-archivo = 'data.xls'
+#! python3     
 #abrimos el documento  
+import xlrd
+archivo="data.xls"
 wb = xlrd.open_workbook(archivo) 
 hoja = wb.sheet_by_index(0) 
 hoja = wb.sheet_by_name('Sheet1') 
 nombres=[]
+horas=[]
+entrada=[]
+salida=[]
+dia=[]
+
+funcionarios=[]
+
 
 for i in range(0,hoja.nrows):
      #se carga un el vector todos los nombres de la columna Nombre
     nombres.append(hoja.cell_value(i,8))
+    dia.append(hoja.cell_value(i,14))
     
-
+    
+    
 #se ordena los registros no repetidos
 nombres = list(set(nombres))
 nombres.remove("NOME")
-entrada=[]
-salida=[]
+dia= list(set(dia))
+#eliminamos los datos que no nos interesan y los datos en blanco
+dia.remove("DT_LEITORA - Dia")
+dia.remove("")
+#print("valor de dia: ", dia ,"cantidad de registro: ", len(dia))
+print("Cantidad de nombres: ", len(nombres))
+#d es de dias
+# h es de hoja de calculo
+# n es de nombre
 
-for i in range(0,hoja.nrows) :
-    for n in range(len(nombres)):
-        if(hoja.cell_value(i,8) == nombres[n]):
-            #nombre,hora,dia
-            if(hoja.cell_value(i,4)=="Entrada"):
-                entrada.append(list([nombres[n],"Entrada",hoja.cell_value(i,11),"Día",hoja.cell_value(i,14)]))
-            elif(hoja.cell_value(i,4)=="Saída"):
-                salida.append(list([nombres[n],"Salida",hoja.cell_value(i,11),"Día",hoja.cell_value(i,14)]))
+for d in range(len(dia)):
+    for h in range(0,hoja.nrows):
+        if(dia[d] == hoja.cell_value(h,14)):
+            for n in range(len(nombres)):
+                if(hoja.cell_value(i,8) == nombres[n]):
+                #nombre,hora,dia
+                    if(hoja.cell_value(h,4)=="Entrada"):
+                       
+                        entrada.append(list([nombres[n],"Entrada",hoja.cell_value(h,11),"Día",hoja.cell_value(h,14)]))
+                        
+                    elif(hoja.cell_value(h,4)=="Saída"):
+                        salida.append(list([nombres[n],"Salida",hoja.cell_value(h,11),"Día",hoja.cell_value(h,14)]))
+
+               
+         
+        
+
+
+
+
 
 mayor=0
 
@@ -39,12 +64,11 @@ for i in range(len(entrada)):
             # print("Entrada: ", entrada[i][i])           
         a=0
 
-print("Salida: ", salida[1])
+print(entrada)
 
 
 print("mayor: ", mayor)
 print("registros de Entradas son: ",len(entrada))
 print("Registros de Salida son: ",len(salida))
-#for i in range(len(nombres)):
 
 input()
