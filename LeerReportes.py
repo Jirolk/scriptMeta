0d82d03479fcb088368502768257cf242b440d69
jirolk <sandrocastillo@unc.edu.py>
2021-09-03 10:08:25 -0400

    Mejoras en el Script con funcion de error

--- LeerReportes.py
+++ LeerReportes.py
@@ -6,14 +6,7 @@ import xlrd
 #os.system("cls")
 #os.system("echo                        Resumen de los datos Extraidos del sistema")
 
-#carga del documento
-archivo = 'download.xls'
-#abrimos el documento  
-wb = xlrd.open_workbook(archivo) 
-hoja = wb.sheet_by_index(0) 
-hoja = wb.sheet_by_name('Folha1') 
-
-#varaibles locales 
+    #varaibles locales 
 sexoF=0;
 sexoM=0;
 para=0;
@@ -50,7 +43,7 @@ def sexoEmpresa(empresa):
     empresaM=0;
     empresaF=0;
     for i in range(0,hoja.nrows):
-  
+
         if (hoja.cell_value(i,5)== empresa
             and hoja.cell_value(i,2)== 'Feminino'):
             empresaF+=1;
@@ -63,7 +56,7 @@ def sexoEmpresa(empresa):
     print("\t\ttiene Hombres: ", empresaM)
     print("\tTotal de Colaboradores: ", empresaM+empresaF)
     print("============================================\n")
-    input()
+    input("Oprima enter para continuar: ")
     #os.system("pause")
 
 
@@ -74,134 +67,154 @@ def alerta():
     #os.system("cls")
     #os.system("echo                        Resumen de los datos Extraidos del sistema")
     #print("\n\t\t\t Los datos a extraerse tienen que ser")
-    #print("\n\t 'Nome'  \t 'DataNascimento' \t 'Sexo' \t 'Nacionalidade' \t 'NaturalidadeUF' \t 'Empresa'\n\n\n\n ")
+    print("\n\t 'Nome'  \t 'DataNascimento' \t 'Sexo' \t 'Nacionalidade' \t 'NaturalidadeUF' \t 'Empresa'\n\n\n\n ")
 
 
 
-
-
-for i in range(0,hoja.nrows):
-    if (i==1):
-        #para tener controlado la iteración
-        break
-    elif(hoja.cell_value(i,0) != 'Nome' or hoja.cell_value(i,1) != 'DataNascimento' 
-        or hoja.cell_value(i,2) != 'Sexo'
-        or hoja.cell_value(i,3) != 'Nacionalidade' or hoja.cell_value(i,4) != 'NaturalidadeUF' 
-        or hoja.cell_value(i,5) != 'Empresa'):
-        alerta()
-        print ("\t\t\t\t\tFijarse que campos faltan")
-        print ("\t '"+hoja.cell_value(i,0)+"'"+" \t '"+hoja.cell_value(i,1)+"'"+" \t '"+hoja.cell_value(i,2)+"'"" \t '"+hoja.cell_value(i,3)+"'"
-               " \t '"+hoja.cell_value(i,4)+"'"+" \t '"+hoja.cell_value(i,5)+"'\n\n\n")
-     #   os.system("pause")
-        break
+try:
     
-    else:      
-        for i in range(0,hoja.nrows):
+    #carga del documento
+    archivo = 'download.xls'
+    #abrimos el documento  
+    wb = xlrd.open_workbook(archivo) 
+    hoja = wb.sheet_by_index(0) 
+    hoja = wb.sheet_by_name('Folha1') 
 
+
+
+
+    for i in range(0,hoja.nrows):
+        if (i==1):
+            #para tener controlado la iteración
+            break
+        elif(hoja.cell_value(i,0) != 'Nome' or hoja.cell_value(i,1) != 'DataNascimento' 
+            or hoja.cell_value(i,2) != 'Sexo'
+            or hoja.cell_value(i,3) != 'Nacionalidade' or hoja.cell_value(i,4) != 'NaturalidadeUF' 
+            or hoja.cell_value(i,5) != 'Empresa'):
+            alerta()
+            print ("\t\t\t\t\tFijarse que campos faltan")
+            print ("\t '"+hoja.cell_value(i,0)+"'"+" \t '"+hoja.cell_value(i,1)+"'"+" \t '"+hoja.cell_value(i,2)+"'"" \t '"+hoja.cell_value(i,3)+"'"
+                " \t '"+hoja.cell_value(i,4)+"'"+" \t '"+hoja.cell_value(i,5)+"'\n\n\n")
+        #   os.system("pause")
+            break
+        
+        else:      
+            for i in range(0,hoja.nrows):
+
+                        
+                #se carga un el vector todos los nombres de la columna Empresa
+                empreNombre.append(hoja.cell_value(i,5))
+
+                #Se calcula la cantidad por sexo y nacionalidad
+                if (hoja.cell_value(i,2)== 'Feminino'):
+                    sexoF+=1;        
+                elif(hoja.cell_value(i,2) == 'Masculino'):
+                    sexoM+=1;
+                if (hoja.cell_value(i,3) == 'PARAGUAYA'
+                    or hoja.cell_value(i,3)== 'Paraguaya'
+                    or hoja.cell_value(i,3)== 'Paraguaio'
+                    or hoja.cell_value(i,3)== 'PARAGUAYO'
+                    or hoja.cell_value(i,3)== 'PARAGAYA'
+                    or hoja.cell_value(i,3)== 'PARAGUAI'):
+                    para+=1;
+                elif (hoja.cell_value(i,3) == 'BRASILEIRA'
+                    or hoja.cell_value(i,3)== 'BRASILEIRO'
+                    or hoja.cell_value(i,3)== 'BRASILERA'
+                    or hoja.cell_value(i,3)== 'BRASILERO'):
+                    bra+=1;
+                elif (hoja.cell_value(i,3) == 'SUIZO'):
+                    sui+=1;
+                elif(hoja.cell_value(i,3)!= 'Nacionalidade'
+                    and hoja.cell_value(i,3)!= 'SUIZO'):
+                    lati+=1
+
+            for i in range(0,hoja.nrows) :
+                #cuenta por ciudades.
+                if(hoja.cell_value(i,4) == 'CONCEPCIÓN'):
+                    concep+=1;
+                elif(hoja.cell_value(i,4) == 'SAN PEDRO'):
+                    sanpedro+=1;
+                elif(hoja.cell_value(i,4) == 'CORDILLERA'):
+                    cordillera+=1;  
+                elif(hoja.cell_value(i,4)== 'GUAIRÁ'):
+                    guaira+=1;
+                elif(hoja.cell_value(i,4)== 'CAAZAPÁ'):
+                    caazapa+=1;
+                elif(hoja.cell_value(i,4)== 'CAAGUAZÚ'):
+                    caaguazu+=1;        
+                elif(hoja.cell_value(i,4)== 'ITAPÚA'):
+                    itapua+=1;
+                elif(hoja.cell_value(i,4)== 'MISIONES'):
+                    misiones+=1;
+                elif(hoja.cell_value(i,4)== 'PARAGUARÍ'):
+                    paraguari+=1;
+                elif(hoja.cell_value(i,4)== 'ALTO PARANÁ'):
+                    altoParana+=1;
+                elif(hoja.cell_value(i,4)== 'CENTRAL'):
+                    central+=1;
+                elif(hoja.cell_value(i,4)== 'ÑEEMBUCÚ'):
+                    neembucu+=1;
+                elif(hoja.cell_value(i,4)== 'AMAMBAY'):
+                    amambay+=1;
+                elif(hoja.cell_value(i,4)== 'CANINDEYÚ'):
+                    canindeyu+=1;
+                elif(hoja.cell_value(i,4)== 'PRESIDENTE HAYES'):
+                    pteHayes+=1;
+                elif(hoja.cell_value(i,4)== 'BOQUERÓN'):
+                    boqueron+=1;
+                elif(hoja.cell_value(i,4)== 'DISTRITO CAPITAL'):
+                    distritoCapi+=1;
+                elif(hoja.cell_value(i,4)== 'ALTO PARAGUAY'):
+                    altoPy+=1;
+                elif (hoja.cell_value(i,4) != 'NaturalidadeUF'):
+                    otroDepart+=1
+
+            #Se extrae solo los datos que no se repiten
+            empreNombre = list(set(empreNombre))
+            empreNombre.remove("Empresa")
+            empreNombre.sort()
+            print("\n\tSe posee actualmente: ", len(empreNombre)," empresas \n son: \n")
+            for i in range(len(empreNombre)):
+                print("\t\t ♦",i+1,") ",empreNombre[i],".")
+            #Se recorre el vector hasta la cantidad de empresas
+            for i in range(len(empreNombre)):
                     
-            #se carga un el vector todos los nombres de la columna Empresa
-            empreNombre.append(hoja.cell_value(i,5))
+                if(empreNombre[i] != "Empresa"):
+                    sexoEmpresa(empreNombre[i])
 
-            #Se calcula la cantidad por sexo y nacionalidad
-            if (hoja.cell_value(i,2)== 'Feminino'):
-                sexoF+=1;        
-            elif(hoja.cell_value(i,2) == 'Masculino'):
-                sexoM+=1;
-            if (hoja.cell_value(i,3) == 'PARAGUAYA'
-                or hoja.cell_value(i,3)== 'Paraguaya'
-                or hoja.cell_value(i,3)== 'Paraguaio'
-                or hoja.cell_value(i,3)== 'PARAGUAYO'
-                or hoja.cell_value(i,3)== 'PARAGAYA'
-                or hoja.cell_value(i,3)== 'PARAGUAI'):
-                para+=1;
-            elif (hoja.cell_value(i,3) == 'BRASILEIRA'
-                or hoja.cell_value(i,3)== 'BRASILEIRO'
-                or hoja.cell_value(i,3)== 'BRASILERA'
-                or hoja.cell_value(i,3)== 'BRASILERO'):
-                bra+=1;
-            elif (hoja.cell_value(i,3) == 'SUIZO'):
-                sui+=1;
-            elif(hoja.cell_value(i,3)!= 'Nacionalidade'
-                and hoja.cell_value(i,3)!= 'SUIZO'):
-                lati+=1
+            
+            print("\n=====================================================================================")
+            print("\t\tConclusión ")
+            print("\tCantidad de Mujeres: ", sexoF, " \n\tCantidad de Hombres: ", sexoM)
+            print("\tCantidad de Paraguayos: ", para)
+            print("\tCantidad de Brasileros: ", bra)
+            print("\tCantidad de Suizos: ", sui)
+            print("\tCantidad de Latinos: ", lati)
+            print("\tCantidad de Colaboradores: ",hoja.nrows-1)
+            print("\n\t De Concepción son: ", concep)
+            print("\t De San Pedro son: ", sanpedro)
+            print("\t De Cordillera son: ", cordillera )
+            print("\t De Guaira son: ", guaira)
+            print("\t De Caazapa son: ", caazapa)
+            print("\t De Caaguazú son: ", caaguazu)
+            print("\t De Itapua son: ",itapua)
+            print("\t De Misiones son: ", misiones)
+            print("\t De Paraguari son: ", paraguari)
+            print("\t De Alto Parana son: ", altoParana)
+            print("\t De Central son: ", central)
+            print("\t De Ñe'embucu son: ", neembucu)
+            print("\t De Amambay son: ", amambay)
+            print("\t De Canindeyu son: ", canindeyu)
+            print("\t De Pte. Hayes son: ", pteHayes)
+            print("\t De Boquerón son: ", boqueron)
+            print("\t De Distrito Capital son: ", distritoCapi)
+            print("\t De Alto Paraguay son: ", altoPy)
+            print("\t De otros Departamentos son: ", otroDepart)
 
-        for i in range(0,hoja.nrows) :
-            #cuenta por ciudades.
-            if(hoja.cell_value(i,4) == 'CONCEPCIÓN'):
-                concep+=1;
-            elif(hoja.cell_value(i,4) == 'SAN PEDRO'):
-                sanpedro+=1;
-            elif(hoja.cell_value(i,4) == 'CORDILLERA'):
-                cordillera+=1;  
-            elif(hoja.cell_value(i,4)== 'GUAIRÁ'):
-                guaira+=1;
-            elif(hoja.cell_value(i,4)== 'CAAZAPÁ'):
-                caazapa+=1;
-            elif(hoja.cell_value(i,4)== 'CAAGUAZÚ'):
-                caaguazu+=1;        
-            elif(hoja.cell_value(i,4)== 'ITAPÚA'):
-                itapua+=1;
-            elif(hoja.cell_value(i,4)== 'MISIONES'):
-                misiones+=1;
-            elif(hoja.cell_value(i,4)== 'PARAGUARÍ'):
-                paraguari+=1;
-            elif(hoja.cell_value(i,4)== 'ALTO PARANÁ'):
-                altoParana+=1;
-            elif(hoja.cell_value(i,4)== 'CENTRAL'):
-                central+=1;
-            elif(hoja.cell_value(i,4)== 'ÑEEMBUCÚ'):
-                neembucu+=1;
-            elif(hoja.cell_value(i,4)== 'AMAMBAY'):
-                amambay+=1;
-            elif(hoja.cell_value(i,4)== 'CANINDEYÚ'):
-                canindeyu+=1;
-            elif(hoja.cell_value(i,4)== 'PRESIDENTE HAYES'):
-                  pteHayes+=1;
-            elif(hoja.cell_value(i,4)== 'BOQUERÓN'):
-                boqueron+=1;
-            elif(hoja.cell_value(i,4)== 'DISTRITO CAPITAL'):
-                distritoCapi+=1;
-            elif(hoja.cell_value(i,4)== 'ALTO PARAGUAY'):
-                altoPy+=1;
-            elif (hoja.cell_value(i,4) != 'NaturalidadeUF'):
-                otroDepart+=1
-
-        #Se extrae solo los datos que no se repiten
-        empreNombre = list(set(empreNombre))
-        #Se recorre el vector hasta la cantidad de empresas
-        for i in range(len(empreNombre)):
-            if(empreNombre[i] != "Empresa"):
-                sexoEmpresa(empreNombre[i])
-        
-        print("\n=====================================================================================")
-        print("\t\tConclusión ")
-        print("\tCantidad de Mujeres: ", sexoF, " \n\tCantidad de Hombres: ", sexoM)
-        print("\tCantidad de Paraguayos: ", para)
-        print("\tCantidad de Brasileros: ", bra)
-        print("\tCantidad de Suizos: ", sui)
-        print("\tCantidad de Latinos: ", lati)
-        print("\tCantidad de Colaboradores: ",hoja.nrows-1)
-        print("\n\t De Concepción son: ", concep)
-        print("\t De San Pedro son: ", sanpedro)
-        print("\t De Cordillera son: ", cordillera )
-        print("\t De Guaira son: ", guaira)
-        print("\t De Caazapa son: ", caazapa)
-        print("\t De Caaguazú son: ", caaguazu)
-        print("\t De Itapua son: ",itapua)
-        print("\t De Misiones son: ", misiones)
-        print("\t De Paraguari son: ", paraguari)
-        print("\t De Alto Parana son: ", altoParana)
-        print("\t De Central son: ", central)
-        print("\t De Ñe'embucu son: ", neembucu)
-        print("\t De Amambay son: ", amambay)
-        print("\t De Canindeyu son: ", canindeyu)
-        print("\t De Pte. Hayes son: ", pteHayes)
-        print("\t De Boquerón son: ", boqueron)
-        print("\t De Distrito Capital son: ", distritoCapi)
-        print("\t De Alto Paraguay son: ", altoPy)
-        print("\t De otros Departamentos son: ", otroDepart)
-
-        print("=======================================================================================\n")
-input("ndeeeeeeeeeee")
-        #os.system("pause")
-        
+            print("=======================================================================================\n")
+    input("")
+            #os.system("pause")
+except:
+    print("Algo anda mal")
+    alerta()
+    input()            
